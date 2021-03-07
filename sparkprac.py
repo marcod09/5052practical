1627d81662fdb05e59da7b367d99bc743a4d4a5d
WenBo <wenbo.zhao.1998@gmail.com>
2021-03-08 02:18:48 +0800

    added searchByUserId ... returns no. of movies watched and no. of genres watched

--- sparkprac.py
+++ sparkprac.py
@@ -1,6 +1,6 @@
 import pyspark.sql.dataframe
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, count, avg, split, explode, first, max, desc, asc 
+from pyspark.sql.functions import col, count, avg, split, explode, first, max, desc, asc, countDistinct
 
 moviesPath = "../practical/ml-latest-small/movies.csv"
 ratingsPath = "../practical/ml-latest-small/ratings.csv"
@@ -29,7 +29,7 @@ def nTopMovieRating(movies, ratings, n):
     averageRatings = ratings.groupBy("movieId").agg(avg("rating"))
     return movies.join(averageRatings, movies.movieId == averageRatings.movieId).select("avg(rating)", "title").orderBy(col("avg(rating)").desc()).limit(n).collect()
 
-#returning top 10 movies
+#returning top 10 movies watched
 def nTopMovieWatches(ratings):
      moviesDF = (ratings.groupBy('movieId').count()).orderBy(desc('count')).limit(10)
      return movies.join(moviesDF, movies.movieId == moviesDF.movieId).select('title').collect()
@@ -42,6 +42,15 @@ def searchMovieByYear(movies, year, limit):
 def searchMovieByGenre(movies, genre, limit):
     return movies.filter(movies.title.contains(genre)).limit(limit).collect()
 
+#returns number of movies and genres watched given a user's Id
+def searchUserId(movies, ratings, userId):
+    noMovieWatched = ratings.groupBy("userId").agg(count("movieId"))
+    noGenreWatched = movies.alias('a').join(ratings.alias('b'), movies.movieId == ratings.movieId).withColumn("genre",explode(split("genres","\|"))).drop("genres").groupBy("userId").agg(countDistinct("genre"))
+    combined = noMovieWatched.alias('a').join(noGenreWatched.alias('b'), noMovieWatched.userId == noGenreWatched.userId).select("a.userId", "a.count(movieId)","b.count(genre)")
+    return combined.filter(combined.userId.contains(userId)).collect()
+
+
+#====================TESTS===================
 # printing out result of nTopMovie ratings
 for x in nTopMovieRating(movies, ratings, 10):
     print(x)
@@ -56,3 +65,5 @@ for x in searchMovieByGenre(movies, "Comedy", 10):
 
 #printing top10 movies
 print(nTopMovieWatches(ratings))
+
+print(searchUserId(movies, ratings, 471))
