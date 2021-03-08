e9abf6f8407f61bee9e47787cb29798a6c95a3b2
WenBo <wenbo.zhao.1998@gmail.com>
2021-03-08 22:42:08 +0800

    reorded tests and methods to reflect ordering in spec sheet

--- sparkprac.py
+++ sparkprac.py
@@ -24,6 +24,23 @@ for x in movies.take(1):
 #cecking schema for ratings
 ratings.printSchema()
 
+#returns number of movies and genres watched given a user's Id
+def searchUserId(movies, ratings, userId):
+    noMovieWatched = ratings.groupBy("userId").agg(count("movieId"))
+    noGenreWatched = movies.alias('a').join(ratings.alias('b'), movies.movieId == ratings.movieId).withColumn("genre",explode(split("genres","\|"))).drop("genres").groupBy("userId").agg(countDistinct("genre"))
+    combined = noMovieWatched.alias('a').join(noGenreWatched.alias('b'), noMovieWatched.userId == noGenreWatched.userId).select("a.userId", "a.count(movieId)","b.count(genre)")
+    return combined.filter(combined.userId.contains(userId)).collect()
+
+#TODO insert new method here
+
+#returns list of movies by user inputed genre... limit it temp
+def searchMovieByGenre(movies, genre, limit):
+    return movies.filter(movies.title.contains(genre)).limit(limit).collect()
+
+#returns list of movies by user inputed year... limit it temp
+def searchMovieByYear(movies, year, limit):
+    return movies.filter(movies.title.contains(year)).limit(limit).collect()
+
 #returning top n movies by rating in descending order
 def nTopMovieRating(movies, ratings, n):
     averageRatings = ratings.groupBy("movieId").agg(avg("rating"))
@@ -34,31 +51,25 @@ def nTopMovieWatches(movies, ratings, n):
      moviesDF = (ratings.groupBy('movieId').count()).orderBy(desc('count')).limit(n)
      return movies.join(moviesDF, movies.movieId == moviesDF.movieId).select('title').collect()
 
-#returns list of movies by user inputed year... limit it temp
-def searchMovieByYear(movies, year, limit):
-    return movies.filter(movies.title.contains(year)).limit(limit).collect()
-
-#returns list of movies by user inputed genre... limit it temp
-def searchMovieByGenre(movies, genre, limit):
-    return movies.filter(movies.title.contains(genre)).limit(limit).collect()
-
-#returns number of movies and genres watched given a user's Id
-def searchUserId(movies, ratings, userId):
-    noMovieWatched = ratings.groupBy("userId").agg(count("movieId"))
-    noGenreWatched = movies.alias('a').join(ratings.alias('b'), movies.movieId == ratings.movieId).withColumn("genre",explode(split("genres","\|"))).drop("genres").groupBy("userId").agg(countDistinct("genre"))
-    combined = noMovieWatched.alias('a').join(noGenreWatched.alias('b'), noMovieWatched.userId == noGenreWatched.userId).select("a.userId", "a.count(movieId)","b.count(genre)")
-    return combined.filter(combined.userId.contains(userId)).collect()
-
+#=======Part 2 Methods========
 #returns a pandas series with result of favourite genre for a given user
 def findFavGenre(movies, ratings, userId):
     countPerGenreWatched = movies.alias('a').join(ratings.alias('b'), movies.movieId == ratings.movieId).withColumn("genre",explode(split("genres","\|"))).drop("genres").groupBy("userId", "genre").count().groupBy("userId").pivot("genre").agg(max("count"))
     countPandas = countPerGenreWatched.filter(countPerGenreWatched.userId.contains(userId)).toPandas()
     return countPandas.drop("userId", 1).idxmax(axis=1)
 
-#====================TESTS===================
-# printing out result of nTopMovie ratings
-print("---printing out result of nTopMovie ratings---")
-for x in nTopMovieRating(movies, ratings, 10):
+
+
+#====================Part 1 TESTS===================
+#printing result of searchUserId
+print("---printing result of searchUserId---")
+print(searchUserId(movies, ratings, 471))
+
+#TODO insert new method test here
+
+#printing out result of searchMovieByGenre
+print("--printing out result of searchMovieByGenre--")
+for x in searchMovieByGenre(movies, "Comedy", 10):
     print(x)
 
 #printing out result of searchMovieByYear
@@ -66,9 +77,14 @@ print("---printing out result of searchMovieByYear---")
 for x in searchMovieByYear(movies, "1992", 10):
     print(x)
 
-#printing out result of searchMovieByGenre
-print("--printing out result of searchMovieByGenre--")
-for x in searchMovieByGenre(movies, "Comedy", 10):
+# printing out result of nTopMovie ratings
+print("---printing out result of nTopMovie ratings---")
+for x in nTopMovieRating(movies, ratings, 10):
+    print(x)
+
+#printing nTopWatchMovies
+print("---printing nTopWatchMovies---")
+for x in nTopMovieWatches(movies, ratings, 5):
     print(x)
 
 #printing Given a list of genres, search all movies belonging to each genre
@@ -79,15 +95,7 @@ for i in range(length):
     for x in searchMovieByGenre(movies, genreList[i], 5):
             print(x)
 
-#printing nTopWatchMovies
-print("---printing nTopWatchMovies---")
-for x in nTopMovieWatches(movies, ratings, 5):
-    print(x)
-
-#printing result of serachUserId
-print("---printing result of serachUserId---")
-print(searchUserId(movies, ratings, 471))
-
+#==========TESTS for Part 2========
 #printing result of findFavGenre given user Id ... here testing user 118
 print("---printing result of findFavGenre given user Id ... here testing user 118---")
 print(findFavGenre(movies, ratings, "118")[0])
